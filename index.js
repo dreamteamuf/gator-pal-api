e84a907552739cb923bf57bcf48f5556a18921cd
Marc Diaz <marcgabe15@gmail.com>
2019-10-11 09:14:34 -0400

    added error handling

--- index.js
+++ index.js
@@ -21,30 +21,35 @@ app.put('/', async (req,res) => {
     let query_params = req.query
     let updated_data = req.body;
     let documentref = db.doc(`patients/${query_params.id}`)
-    let data_keys = Object.keys(updated_data)
-    if (data_keys === null || data_keys === undefined) {
+    let data_keys = Object.keys(updated_data) //grabs the keys of the updated data
+
+    if (data_keys === null || data_keys === undefined) { //empty data
         res.status(400).send('bad input')
     }
+    
     try {
-        await documentref.get().then((documentSnapshot) => {
+        let data = null
+        
+        await documentref.get().then((documentSnapshot) => {//gets the data
             if (!documentSnapshot.exists) {
                 throw new Error('cannot find input')
             }
             console.log(`Found document at '${documentSnapshot.ref.path}'`);
-            let data = documentSnapshot.data()
+            data = documentSnapshot.data()
             let data_keys = Object.keys(updated_data);
 
-            for (let key of data_keys) {
+            for (let key of data_keys) { //updates the data for every update key
                 if (data[key] === null || data[key] === undefined) {
                     throw new Error('input key is not in the snapshot')
                 }
                 data[key] = updated_data[key]
             }
-            console.log(data)
-            res.status(200).send(data)
-        });
-    } catch(error) {
-        console.log(`Error ${error}`)
+        })
+        await documentref.set(data).then((result) => { //sets the new data
+            res.status(200).send(result)
+        })
+    } catch(error) { //catches error
+        console.log(`${error}`)
         res.status(500).send(error)
     }
 })
