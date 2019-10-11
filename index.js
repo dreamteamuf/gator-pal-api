5a3e2f33b5459ad3d0eccd7da72badd1806cad7d
Marc Diaz <marcgabe15@gmail.com>
2019-10-11 08:39:35 -0400

    PUT Request

--- index.js
+++ index.js
@@ -11,6 +11,44 @@ let db = admin.firestore();
 const app = require('express')()
 const server = require('http').Server(app)
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.put('/', async (req,res) => {
+    let query_params = req.query
+    let updated_data = req.body;
+    let documentref = db.doc(`patients/${query_params.id}`)
+    let data_keys = Object.keys(updated_data)
+    if (data_keys === null || data_keys === undefined) {
+        res.status(400).send('bad input')
+    }
+    try {
+        await documentref.get().then((documentSnapshot) => {
+            if (!documentSnapshot.exists) {
+                throw new Error('cannot find input')
+            }
+            console.log(`Found document at '${documentSnapshot.ref.path}'`);
+            let data = documentSnapshot.data()
+            let data_keys = Object.keys(updated_data);
+
+            for (let key of data_keys) {
+                if (data[key] === null || data[key] === undefined) {
+                    throw new Error('input key is not in the snapshot')
+                }
+                data[key] = updated_data[key]
+            }
+            console.log(data)
+            res.status(200).send(data)
+        });
+    } catch(error) {
+        console.log(`Error ${error}`)
+        res.status(500).send(error)
+    }
+})
+
 server.listen(8080, () => {
     const host = server.address().address
     const port = server.address().port
